49fc8fd5aaf51adab1b96e31b9ee209fa2120d98
Pranav Sankhe <pranavsankhe40@gmail.com>
2016-12-04 22:03:22 +0530

    fixed errors

--- script.py
+++ script.py
@@ -1,62 +1,125 @@
-import os  
-import numpy as np  
-import pandas as pd  
+import numpy as np 
+import os 
 import matplotlib.pyplot as plt
+import pandas as pd 
+import fuzzywuzzy
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from datetime import datetime
+import itertools as it
+#checking the type of file
+path = os.getcwd() + '/testdata.csv'
 
 
-path = os.getcwd() + '/testdata.txt' 
-data = pd.read_csv(path, header=1, names=['Customer_ID','Age', 'Martial status','Turnover time','Buisness amount'])           #read data from the file 
-#data = pd.read_csv(path, header=1, names=['Customer_ID','company name', 'group_key_promoters','activity_company','contact_company','authorized_person_mobile','address_company','customer_creation_date','meeting_date','amount'])           #read data from the file 
+def file_type():
+    extension = os.path.splitext(os.path.basename(path))[1] 
+    
+    extensions = ['.txt', '.csv', 'xlsx' ]
+    
+    if extension == extensions[0]:
+        return 0
+    if extension == extensions[1]:
+        return 1 
+    if extension == extensions[2]:
+        return 2 
 
-#print("before normalization")
-print(data.head())  #to display first few rows.
+def str2int(s, chars):
+    i = 0
+    for c in reversed(s):
+        i *= len(chars)
+        i += chars.index(c)
+    return i
 
-#feature(variable) normalization 
+chars = "".join(str(n) for n in range(10)) + "ABCDEFGHIJKLMNOPQRSTUVWXYZ-"
+    
 
-data = (data - data.mean()) / data.std()  
-data.insert(0, 'Ones', 1)
-print(data.mean())
-print(data.std())
+def load_data():
+    if file_type() == 0|1:
+        data = pd.read_csv(path, delimiter=None, header='infer')
 
-#print("after normalization")
-print(data.head())
+        namelist = data.columns.values.tolist()
+        
+
+        id_match = np.zeros(len(namelist))
+        amount_match = np.zeros(len(namelist))
+        turnoverTime_match = np.zeros(len(namelist))
+        create_date_match = np.zeros(len(namelist))
+        meet_date_match = np.zeros(len(namelist))
+        
+        for i in range(len(namelist)):
+            id_match[i] = fuzz.token_set_ratio("id",namelist[i])
+        
+            amount_match[i] = max(fuzz.token_set_ratio("total",namelist[i]),fuzz.token_set_ratio("amount",namelist[i]))
+            #turnoverTime_match = fuzz.token_set_ratio("",namelist[i])
+            create_date_match[i] =  max(fuzz.token_set_ratio("creation",namelist[i]), fuzz.token_set_ratio("start",namelist[i]))
+            meet_date_match[i] = max(fuzz.token_set_ratio("meeting",namelist[i]),fuzz.token_set_ratio("customer meeting",namelist[i]) )
+            
+            customer_id_index = np.argmax(id_match, axis=None, out=None)
+            amount_index =  np.argmax(amount_match, axis=None, out=None)
+            create_date_index = np.argmax(create_date_match, axis=None, out=None)
+            meet_date_index = np.argmax(meet_date_match, axis=None, out=None)
+            
+        
+        customer_id = data.iloc[:,customer_id_index:customer_id_index+1]
+        amount = data.iloc[:,amount_index:amount_index+1]
+        create_date =  data.iloc[:,create_date_index:create_date_index+1]
+        meet_date = data.iloc[:,meet_date_index:meet_date_index+1]
+        
+        return customer_id, amount, create_date, meet_date,namelist
 
 
-# set X (training data) and y (target variable)
-cols = data.shape[1]  
-del data['Customer_ID']
-cols_update = data.shape[1]  
-X = data.iloc[:,0:cols-3]  
-y = data.iloc[:,cols_update-2:cols_update]
 
-# convert to matrices and initialize theta
-X = np.matrix(X.values) 
+startdate_array = load_data()[2].as_matrix()
+meetdate_array =  load_data()[3].as_matrix()
+
+turnoverTime = np.zeros(len(startdate_array))
+
+
+#print(startdate_array[0,:][0])
+
+
+try:
+    for i in range(max(len(startdate_array), len(meetdate_array) )):  
+        d1 = datetime.strptime((startdate_array[i,:][0].split(" ")[0]), "%Y/%m/%d")
+        d2 = datetime.strptime((meetdate_array[i,:][0]), "%Y/%m/%d")
+        turnoverTime[i] = abs((d2 - d1).days)  
+except AttributeError:
+    pass
+
+
+
+customer_id = load_data()[0].as_matrix() 
+amount = load_data()[1].as_matrix()
+amount_mean =np.mean(amount) 
+amount_var = np.std(amount)
+amount = (amount - np.mean(amount) )/ np.std(amount)
+
+for i in range(len(customer_id)):
+    customer_id[i] = str2int(customer_id[i,:][0], chars) 
+
  
-y = np.matrix(y.values)  
-theta = np.matrix(np.array([0,0,0]))
+customer_id = (customer_id - np.mean(customer_id)) / np.std(customer_id)
 
-y_turnover_time = y[:,0]
-y_amount = y[:,1]
+turnoverTime_mean = np.mean(turnoverTime)
+turnoverTime_var = np.std(turnoverTime)
+turnoverTime =  (turnoverTime - np.mean(turnoverTime)) / np.std(turnoverTime)
 
-#print(np.squeeze(np.asarray(y_turnover_time)).sum())
-#print(y_amount.shape)
+theta = np.matrix(np.array([0,0]))
+X = np.column_stack((np.ones(len(customer_id)), customer_id) )
 
 
-
-#cost function
 def computeCost(X, y, theta):                                                   
     inner = np.power(((X * theta.T) - y), 2)
     return np.sum(inner) / (2 * len(X))
 
+
+ 
 def gradientDescent(X, y, theta, alpha, iters):  
     temp = np.matrix(np.zeros(theta.shape))
-    
-    # .ravel() flattens a matrix into a 1-D array 
-    # number of parameters = parameters
-    parameters = int(theta.ravel().shape[1])                                   
+    parameters = int(theta.ravel().shape[1])
     cost = np.zeros(iters)
 
-    for i in range(iters):             
+    for i in range(iters):
         error = (X * theta.T) - y
 
         for j in range(parameters):
@@ -68,25 +131,17 @@ def gradientDescent(X, y, theta, alpha, iters):
 
     return theta, cost
 
-alpha = 0.02       #learning rate  
+alpha = 0.01       #learning rate  
 iters = 1000      #no. of iterations
+g_turnoverTime, cost_turnoverTime = gradientDescent(X, turnoverTime, theta, alpha, iters)
+g_amount, cost_amount = gradientDescent(X, amount, theta, alpha, iters)
 
-g_turnover_time, cost_turnover_time = gradientDescent(X, y_turnover_time, theta, alpha, iters)
-g_amount, cost_amount = gradientDescent(X, y_amount, theta, alpha, iters)
+x = np.linspace(np.amin(X[:,1]), np.amax(X[:,1]), 100)
 
-x_age = np.linspace(np.amin(X[:,1]), np.amax(X[:,1]), 100)
-x_martialStatus = np.linspace(np.amin(X[:,2]), np.amax(X[:,2]), 100)
 
-f_turnover_time = g_turnover_time[0,0] + g_turnover_time[0,1]*x_age + g_turnover_time[0,2]*x_martialStatus   
-f_amount =  g_amount[0,0] + g_amount[0,0]*x_age + g_amount[0,0]*x_martialStatus 
-
-#print(f_turnover_time)
-#print(f_amount)
-turnover = (f_turnover_time + data.mean()[3])*data.std()[3]
-#amount = (f_amount + data.mean()[4])*data.std()[4]
-
-#print(turnover)
-#print(amount)
-
+f_turnoverTime = g_turnoverTime[0,0] + g_turnoverTime[0,1]*x
+f_amount =  g_amount[0,0] + g_amount[0,0]*x  
+result_turnover_time = f_turnoverTime*turnoverTime_var + turnoverTime_mean
+result_amount = f_amount*amount_var + amount_mean
 
 
